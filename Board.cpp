c216a1aaf809fa28c977e59505b363b28e4336fc
root <awall@chapman.edu>
2020-03-05 22:15:01 +0000

    commit of Board class after density

--- Board.cpp
+++ Board.cpp
@@ -10,9 +10,14 @@ using namespace std;
   }
 
 
-  void Board::createMap(int r, int c){
+  void Board::createMap(int r, int c, float d){
   numRows = r;
   numColumns = c;
+  density = d;
+  double a = 0;
+  int counter = 0;
+  int maxValue = d*(numRows*numColumns);
+
   char** myGrid;
   myGrid = new char*[numRows];
   for(int i = 0;i < numRows;++i){
@@ -21,7 +26,16 @@ using namespace std;
 
   for(int i = 0; i < numRows; ++i){
     for(int j = 0; j < numColumns;++j){
-      myGrid[i][j] = '-';
+      a = ((double) rand() / (RAND_MAX));
+      if(a<d && counter<maxValue){
+        myGrid[i][j]='X';
+        counter++;
+      }
+      else{
+        myGrid[i][j] = '-';
+      }
+
+
     }
   }
 
@@ -36,8 +50,8 @@ using namespace std;
   delete myGrid;
 }
 
-// int main(int argc, char const *argv[]) {
-//   Board *b = new Board();
-//   b->createMap(10,7);
-//   return 0;
-// }
+int main(int argc, char const *argv[]) {
+  Board *b = new Board();
+  b->createMap(10,7,.5);
+  return 0;
+}
