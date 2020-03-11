549b44f42cd17519b7ea5dbb21338a7b46382bb6
root <awall@chapman.edu>
2020-03-11 06:12:57 +0000

    this commit has all different game modes working and running

--- GameMode.h
+++ GameMode.h
@@ -6,6 +6,7 @@ using namespace std;
 class GameMode : public Board{
 public:
   GameMode();
+  // GameMode()
   ~GameMode();
   int numRows;
   int numColumns;
--- GameModeClass.cpp
+++ GameModeClass.cpp
@@ -1,8 +1,11 @@
 #include "GameMode.h"
 
 GameMode::GameMode(){
-  // Board board = new Board();
-  // createMap(10,7,.5);
+    // Board board = new Board();
+    // createMap(10,7,.5);
+    int numRows = 0;
+    int numColumns = 0;
+    double density= 0;
 }
 
 GameMode::~GameMode(){
@@ -19,6 +22,7 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
   }
 
   //check for neighbors
+  cout<<"NORMAL MODE "<<endl;
   int counter = 0;
   for(int i =0;i<numRows;++i){
     for(int j = 0;j<numColumns;++j){
@@ -29,10 +33,10 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
           // cout<<"counting"<<endl;
               }
         if(arrGrid[i-1][j-1]=='X'){
-          cout<<"I AM COUNTING DOWN AND LEFT"<<endl;
+          // cout<<"I AM COUNTING DOWN AND LEFT"<<endl;
 
-          counter+1;
-          cout<<"counter after entering: "<<counter<<endl;
+          counter++;
+          // cout<<"counter after entering: "<<counter<<endl;
               }
         if(arrGrid[i][j-1]=='X'){
           counter++;
@@ -56,9 +60,9 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
 
 
       else if(i==0 && j==0 ){//top left corner
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i][j+1]=='X'){ //to the right
           counter++;
         }
@@ -71,9 +75,9 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
       }
 
       else if(i==0 && j ==(numColumns-1)){ //top right corner
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i][j-1]=='X'){ //to the left
           counter++;
         }
@@ -86,9 +90,9 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
       }
 
       else if(i ==(numRows-1) && j ==0){//bottom left corner
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
         }
@@ -101,9 +105,9 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
       }
 
       else if(i ==(numRows-1)&& j ==(numColumns-1)){ //bottom right corner
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
         }
@@ -114,10 +118,10 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if(i == 0) && (j != numColumns-1 || j != 0)){ //middle top
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+      else if((i == 0) && (j != numColumns-1 && j != 0)){ //top row
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i][j-1]=='X'){ //to the left
           counter++;
         }
@@ -134,10 +138,10 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if(i == numRows-1) && (j != numColumns - 1 || j != 0)){ //middle bottom
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+      else if((i == numRows-1) && (j != numColumns-1 && j != 0)){ //bottom row
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i-1][j-1]=='X'){ //left diagonal
           counter++;
         }
@@ -154,10 +158,10 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if((i != 0 || i != numRows-1) && (j == 0)){ //middle left side
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+      else if((i != 0 && i != numRows-1) && (j == 0)){ // left column
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
         }
@@ -174,10 +178,10 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if((i != 0 || i != numRows-1) && (j == numColumns-1)){ //middle right side
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+      else if((i != 0 && i != numRows-1) && (j == numColumns-1)){ //far right column
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
         }
@@ -196,7 +200,7 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
       }
 
       //for checking
-      cout<<"counter: "<<counter<<endl;
+      // cout<<"counter: "<<counter<<endl;
 
       //fill new generation based off counter
       if(counter <=1){
@@ -211,14 +215,24 @@ void GameMode::normalMode(int numRows, int numColumns, double density){
       }
     } //exit j for loop
   } //exit i for loop
+
+
   cout<<"new generation: "<<endl;
-  for(int i = 0;i<numRows;i++){
-    for(int j = 0;j<numColumns;j++){
+  for(int i = 0;i<numRows;++i){
+    for(int j = 0;j<numColumns;++j){
       cout<<nextGeneration[i][j];
     }
     cout<<endl;
   }
 }
+
+
+
+
+
+
+
+
 
 void GameMode::doughnutMode(int numRows, int numColumns, double density){
   char **arrGrid = Board::createMap(numRows, numColumns, density);
@@ -237,13 +251,325 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
       if(i!=0 && i!=(numRows-1) && j!= 0 && j!= (numColumns-1)){//middle squares
         if(arrGrid[i-1][j]=='X'){
           counter++;
+
+              }
+        if(arrGrid[i-1][j-1]=='X'){
+
+
+          counter+1;
+          // cout<<"counter after entering: "<<counter<<endl;
+              }
+        if(arrGrid[i][j-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[i+1][j-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){
+          counter++;
+        }
+        if(arrGrid[i+1][j+1]=='X'){
+          counter++;
+        }
+        if(arrGrid[i][j+1]=='X'){
+          counter++;
+        }
+        if(arrGrid[i-1][j+1]=='X'){
+          counter++;
+        }
+      }
+
+      else if(i==0 && j==0 ){ //top left corner
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i][j+1]=='X'){ //to the right
+          counter++;
+        }
+        if(arrGrid[i+1][j+1]=='X'){ //right bottom diagonal
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[0][0]){
+          counter++;
+        }
+
+      }
+
+      else if(i==0 && j ==(numColumns-1)){ //top right corner
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i][j-1]=='X'){ //to the left
+          counter++;
+        }
+        if(arrGrid[i+1][j-1]=='X'){ //left bottom diagonal
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[numRows-1][j]=='X'){
+          counter++;
+        }
+        if(arrGrid[numRows-1][j-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[numRows-1][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[i+1][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][0]=='X'){
+          counter++;
+        }
+
+      }
+
+      else if(i ==(numRows-1) && j ==0){//bottom left corner
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i-1][j]=='X'){ //above
+          counter++;
+        }
+        if(arrGrid[i-1][j+1]=='X'){ //right diagonal
+          counter++;
+        }
+        if(arrGrid[i][j+1]=='X'){ //to the right
+          counter++;
+        }
+        if(arrGrid[i][numColumns-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[i-1][numColumns-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][numColumns-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j+1]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j]=='X'){
+          counter++;
+        }
+
+      }
+
+      else if(i ==(numRows-1)&& j ==(numColumns-1)){ //bottom right corner
+        //
+        if(arrGrid[i-1][j]=='X'){ //above
+          counter++;
+        }
+        if(arrGrid[i-1][j-1]=='X'){ //left diagonal
+          counter++;
+        }
+        if(arrGrid[i][j-1]=='X'){ //to the left
+          counter++;
+        }
+        if(arrGrid[0][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j]=='X'){
+          counter++;
+        }
+        if(arrGrid[i][0]){
+          counter++;
+        }
+        if(arrGrid[i-1][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j-1]){
+          counter++;
+        }
+
+
+      }
+
+      else if((i == 0) && (j != numColumns-1 && j != 0)){ // top row
+
+        if(arrGrid[i][j-1]=='X'){ //to the left
+          counter++;
+        }
+        if(arrGrid[i][j+1]=='X'){ //to the right
+          counter++;
+        }
+        if(arrGrid[i+1][j-1]=='X'){ //left diagonal
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[i+1][j+1]=='X'){ //right diagonal
+          counter++;
+        }
+        if(arrGrid[numRows-1][j-1]=='X'){
+          counter++;
+        }
+        if(arrGrid[numRows-1][j]=='X'){
+          counter++;
+        }
+        if(arrGrid[numRows-1][j+1]=='X'){
+          counter++;
+        }
+
+
+      }
+
+      else if((i == numRows-1) && (j != numColumns - 1 && j != 0)){ // bottom row
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i][j+1]=='X'){ //left diagonal
+          counter++;
+        }
+        if(arrGrid[i][j-1]=='X'){ //above
+          counter++;
+        }
+        if(arrGrid[i-1][j-1]=='X'){ //right diagonal
+          counter++;
+        }
+        if(arrGrid[i-1][j]=='X'){ //to the left
+          counter++;
+        }
+        if(arrGrid[i-1][j+1]=='X'){ //to the right
+          counter++;
+        }
+        if(arrGrid[0][j+1]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j]=='X'){
+          counter++;
+        }
+        if(arrGrid[0][j-1]=='X'){
+          counter++;
+        }
+      }
+
+      else if((i != 0 && i != numRows-1) && (j == 0)){ //left side
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i-1][j]=='X'){ //above
+          counter++;
+        }
+        if(arrGrid[i-1][j+1]=='X'){ //right above diagonal
+          counter++;
+        }
+        if(arrGrid[i][j+1]=='X'){ //to the right
+          counter++;
+        }
+        if(arrGrid[i+1][j+1]=='X'){ //right below diagonal
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[i][numColumns-1]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[i-1][numColumns-1]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[i+1][numColumns-1]=='X'){ //below
+          counter++;
+        }
+      }
+
+      else if((i != 0 && i != numRows-1) && (j == numColumns-1)){ //right side
+        // if(arrGrid[i][j]=='X'){
+        //   counter++;
+        // }
+        if(arrGrid[i-1][j]=='X'){ //above
+          counter++;
+        }
+        if(arrGrid[i-1][j-1]=='X'){ //left above diagonal
+          counter++;
+        }
+        if(arrGrid[i][j-1]=='X'){ //to the left
+          counter++;
+        }
+        if(arrGrid[i+1][j-1]=='X'){ //left below diagonal
+          counter++;
+        }
+        if(arrGrid[i+1][j]=='X'){ //below
+          counter++;
+        }
+        if(arrGrid[i-1][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[i][0]=='X'){
+          counter++;
+        }
+        if(arrGrid[i+1][0]=='X'){
+          counter++;
+        }
+      }
+
+      //for checking
+      // cout<<"counter: "<<counter<<endl;
+
+      //fill new generation based off counter
+      if(counter <=1){
+        nextGeneration[i][j]='-';
+      }else if(counter == 2){
+        nextGeneration[i][j]= arrGrid[i][j];
+      }else if(counter ==3){
+        nextGeneration[i][j]= 'X';
+      }
+      else{
+        nextGeneration[i][j]= '-';
+      }
+    } //exit j for loop
+  } //exit i for loop
+  cout<<endl;
+  cout<<"new generation: "<<endl;
+  cout<<endl;
+
+  for(int i = 0;i<numRows;i++){
+    for(int j = 0;j<numColumns;j++){
+      cout<<nextGeneration[i][j];
+    }
+    cout<<endl;
+  }
+}
+
+
+
+
+
+
+
+
+void GameMode::mirrorMode(int numRows, int numColumns, double density){
+  char **arrGrid = Board::createMap(numRows, numColumns, density);
+
+  char** nextGeneration;
+  nextGeneration = new char*[numRows];
+  for(int p = 0;p < numRows;++p){
+    nextGeneration[p]= new char[numColumns];
+  }
+
+  //check for neighbors
+  cout<<"MIRROR MODE "<<endl;
+  int counter = 0;
+  for(int i =0;i<numRows;++i){
+    for(int j = 0;j<numColumns;++j){
+      counter = 0;
+      if(i!=0 && i!=(numRows-1) && j!= 0 && j!= (numColumns-1)){//middle squares
+        if(arrGrid[i-1][j]=='X'){
+          counter++;
           // cout<<"counting"<<endl;
               }
         if(arrGrid[i-1][j-1]=='X'){
-          cout<<"I AM COUNTING DOWN AND LEFT"<<endl;
+          // cout<<"I AM COUNTING DOWN AND LEFT"<<endl;
 
-          counter+1;
-          cout<<"counter after entering: "<<counter<<endl;
+          counter++;
+          // cout<<"counter after entering: "<<counter<<endl;
               }
         if(arrGrid[i][j-1]=='X'){
           counter++;
@@ -267,40 +593,54 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
 
 
       else if(i==0 && j==0 ){//top left corner
-        if(arrGrid[i][j]=='X'){
-          counter++;
-        }
+        // if(arrGrid[i][j]=='X'){
+        //   counter+=3;
+        // }
         if(arrGrid[i][j+1]=='X'){ //to the right
           counter++;
         }
-        if(arrGrid[i+1][j+1]=='X'){ //right bottom diagonal
+        if(arrGrid[i+1][j+1]=='X'){ //right diagonal
           counter++;
         }
         if(arrGrid[i+1][j]=='X'){ //below
           counter++;
         }
-        if(arrGrid[i+(numRows-1)][j+1]=='X'){ //top right diagonal
+        if(arrGrid[numRows-1][numColumns-1]=='X'){
           counter++;
         }
-        if(arrGrid[i+(numRows-1)][j]=='X'){ //above
+        if(arrGrid[i][numColumns-1]=='X'){
           counter++;
         }
-        if(arrGrid[i][j+ (numColumns-1)]=='X'){ //to the left
+        if(arrGrid[i+1][numColumns-1]=='X'){
           counter++;
         }
-        if(arrGrid[i+1][j+ (numColumns-1)]=='X'){ //bottom left diagonal
+        if(arrGrid[numRows-1][j]=='X'){
           counter++;
         }
+        if(arrGrid[numRows-1][j+1]=='X'){
+          counter++;
+        }
+
+
+
+
+
+
+
+
+
+
+
       }
 
       else if(i==0 && j ==(numColumns-1)){ //top right corner
         if(arrGrid[i][j]=='X'){
-          counter++;
+          counter+=3;
         }
         if(arrGrid[i][j-1]=='X'){ //to the left
           counter++;
         }
-        if(arrGrid[i+1][j-1]=='X'){ //left bottom diagonal
+        if(arrGrid[i+1][j-1]=='X'){ //left diagonal
           counter++;
         }
         if(arrGrid[i+1][j]=='X'){ //below
@@ -310,7 +650,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
 
       else if(i ==(numRows-1) && j ==0){//bottom left corner
         if(arrGrid[i][j]=='X'){
-          counter++;
+          counter+=3;
         }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
@@ -325,7 +665,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
 
       else if(i ==(numRows-1)&& j ==(numColumns-1)){ //bottom right corner
         if(arrGrid[i][j]=='X'){
-          counter++;
+          counter+=3;
         }
         if(arrGrid[i-1][j]=='X'){ //above
           counter++;
@@ -337,7 +677,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if(i == 0) && (j != numColumns-1 || j != 0)){ //middle top
+      else if((i == 0) && (j != numColumns-1 && j != 0)){ //top row
         if(arrGrid[i][j]=='X'){
           counter++;
         }
@@ -357,7 +697,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if(i == numRows-1) && (j != numColumns - 1 || j != 0)){ //middle bottom
+      else if((i == numRows-1) && (j != numColumns-1 && j != 0)){ //bottom row
         if(arrGrid[i][j]=='X'){
           counter++;
         }
@@ -377,7 +717,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if((i != 0 || i != numRows-1) && (j == 0)){ //middle left side
+      else if((i != 0 && i != numRows-1) && (j == 0)){ // left column
         if(arrGrid[i][j]=='X'){
           counter++;
         }
@@ -397,7 +737,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
           counter++;
         }
       }
-      else if((i != 0 || i != numRows-1) && (j == numColumns-1)){ //middle right side
+      else if((i != 0 && i != numRows-1) && (j == numColumns-1)){ //far right column
         if(arrGrid[i][j]=='X'){
           counter++;
         }
@@ -419,7 +759,7 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
       }
 
       //for checking
-      cout<<"counter: "<<counter<<endl;
+      // cout<<"counter: "<<counter<<endl;
 
       //fill new generation based off counter
       if(counter <=1){
@@ -434,20 +774,19 @@ void GameMode::doughnutMode(int numRows, int numColumns, double density){
       }
     } //exit j for loop
   } //exit i for loop
+
+
   cout<<"new generation: "<<endl;
-  for(int i = 0;i<numRows;i++){
-    for(int j = 0;j<numColumns;j++){
+  for(int i = 0;i<numRows;++i){
+    for(int j = 0;j<numColumns;++j){
       cout<<nextGeneration[i][j];
     }
     cout<<endl;
   }
 }
 
-
-
-
 int main(int argc, char const *argv[]) {
   GameMode *g = new GameMode;
-  g->normalMode(3,3,.34);
+  g->doughnutMode(50,50,.5);
   return 0;
 }
